652d2841f1de0b736bbc57f7f2373ea1bf7601c7
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2025-09-22 23:17:11 -0400

    chore: split devenv and nix flake
    splitting build flake and devenv config allows CI workflows to use
    devenv's github actions

--- flake.nix
+++ flake.nix
@@ -3,24 +3,17 @@
     Build package for second-brain application
   '';
   inputs = {
-    devenv.url = "github:cachix/devenv";
     flake-utils.url = "github:numtide/flake-utils";
     naersk.url = "github:nix-community/naersk";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs = {nixpkgs.follows = "nixpkgs";};
-    };
   };
 
   outputs = {
-    self,
     flake-utils,
     naersk,
     nixpkgs,
-    devenv,
     ...
-  } @ inputs:
+  }:
     flake-utils.lib.eachDefaultSystem (
       system: let
         pkgs = (import nixpkgs) {inherit system;};
@@ -36,10 +29,6 @@
           default = naersk'.buildPackage (mkApp true "build");
           debug = naersk'.buildPackage (mkApp false "build");
           test = naersk'.buildPackage (mkApp false "test");
-        };
-        devShell = devenv.lib.mkShell {
-          inherit pkgs inputs;
-          modules = [./devenv.nix];
         };
       }
     );
