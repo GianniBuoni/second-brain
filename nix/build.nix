748e08aa222fc7e2058eb69e2d5a1ed900e01e5a
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2026-02-26 17:41:44 -0500

    chore: add dendritic flake inputs

--- nix/build.nix
+++
@@ -1,9 +0,0 @@
-{inputs, ...}: {
-  imports = with inputs.rust-flake; [
-    flakeModules.default
-    flakeModules.nixpkgs
-  ];
-  perSystem = {self', ...}: {
-    packages.default = self'.packages.second-brain;
-  };
-}
