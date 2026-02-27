38c515ba882d7030589119637ea2a96ba5d73b4e
GianniBuoni <162598175+GianniBuoni@users.noreply.github.com>
2026-02-27 16:24:40 -0500

    refactor: move and simplify ConfigFile newtype
    change ConfigFile from struct to tuple struct

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
-use std::env;
-
-use anyhow::Result;
 use clap::Parser;
 
 use second_brain::prelude::*;
 
-fn main() -> Result<()> {
-    let config_file = ConfigFile::try_from_env(env::var("SECOND_BRAIN_CONFIG"))?.try_build()?;
-    let config = AppConfig::try_from(&config_file)?;
+fn main() -> Result<(), Status> {
+    let config_file = ConfigFile::try_from_env("SECOND_BRAIN_CONFIG")?.try_build()?;
+    let config = AppConfig::try_from(config_file)?;
     let command = Args::parse().command.unwrap_or_default();
 
     let app = second_brain::app::App { config, command };
